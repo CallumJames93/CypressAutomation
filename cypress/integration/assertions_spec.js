d0077c072d72113e5f5c257eeb391ec11e05d544
Callum <callum.gill1993@hotmail.co.uk>
2020-03-19 11:14:28 +1100

    Commit 190320
    Added a new test to assert on a regular expression

--- cypress/integration/assertions_spec.js
+++ cypress/integration/assertions_spec.js
@@ -17,6 +17,21 @@ context('Assertion Tests', () =>{
 
 		})
 
-		//To be continued.
+		it('finds element by class name regex', () => {
+			cy.get('.docs-header')
+			  .find('div')
+			  .should(($div) =>{
+			  	expect($div).to.have.length(1)
+
+			  	const className = $div[0].className
+
+			  	expect(className).to.match(/heading-/)
+
+			  })
+
+			  .then(($div) =>{
+			  	expect($div, 'text content').to.have.text('Introduction')
+			  })
+		})
 	})
 })
